7399c52581ec07c8cbd5e1b867e342eb3bdbe6fc
jeromejaglale <jerome.jaglale@gmail.com>
2022-10-14 21:22:40 +0000

    Task 1 and 2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { COLORS, WEIGHTS } from '../../constants';
+import { COLORS, WEIGHTS, QUERIES } from '../../constants';
 import Logo from '../Logo';
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
+
+import UnstyledButton from '../UnstyledButton';
+import Icon from '../Icon';
+
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -29,7 +33,19 @@ const Header = () => {
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
         </Nav>
-        <Side />
+
+        <MobileActions>
+          <UnstyledButton>
+            <Icon id="shopping-bag" />
+          </UnstyledButton>        
+          <UnstyledButton>
+            <Icon id="search" />
+          </UnstyledButton>        
+          <UnstyledButton>
+            <Icon id="menu" />
+          </UnstyledButton>        
+        </MobileActions>
+ 
       </MainHeader>
 
       <MobileMenu
@@ -46,12 +62,30 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+
+  @media ${QUERIES.tabletAndSmaller} {
+    border-top: 4px solid #313235;
+  }
+
 `;
 
 const Nav = styled.nav`
   display: flex;
   gap: 48px;
   margin: 0px 48px;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
+`;
+
+const MobileActions = styled.nav`
+  display: none;
+
+  @media ${QUERIES.tabletAndSmaller} {
+    display: flex;
+    gap: 23px;
+  }
 `;
 
 const Side = styled.div`
